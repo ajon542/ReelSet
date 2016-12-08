8ea2ea6cfa7ba9e9f10566cb1ebfc0ebe08c2340
Andrew Jones <ajon542@gmail.com>
2016-12-07 21:45:35 -0800

    Added a bunch of comments for prototype code
    There is a tonne of stuff to consider for improvements

--- ReelSetComponent/Assets/Scripts/ReelLayout.cs
+++ ReelSetComponent/Assets/Scripts/ReelLayout.cs
@@ -13,7 +13,15 @@ public class ReelLayout : MonoBehaviour
     private float topSymbolPosition;
     private float symbolEnterPosition;
 
-    // Use this for initialization
+    [SerializeField]
+    private float speed = 0.003f;
+
+    // NOTE: PROTOTYPE CODE ONLY
+    // This code is written mainly to test some ideas out. It is horrible and
+    // I'm not convinced a single animation curve for the reel is the best way
+    // to go at this point.
+
+    // TODO: Draw a gizmo for the animation curve and symbol locations.
     void Start()
     {
         if (symbolPrefab == null)
@@ -21,7 +29,7 @@ public class ReelLayout : MonoBehaviour
             return;
         }
 
-        layoutCurve = AnimationCurve.Linear(0, 0, 1, -10);
+        layoutCurve = AnimationCurve.Linear(0, 5, 1, -5);
 
         // Get the position of the top symbol. This will never change.
         // All other symbol positions will be calculation from this value.
@@ -50,25 +58,33 @@ public class ReelLayout : MonoBehaviour
             float y = layoutCurve.Evaluate(symbolPosition);
             Vector3 currentPos = symbol.transform.position;
 
+            // TODO: Making the assumption the curve just represents y-coord. Though, this
+            // can simply be a single implementation. Other implementations can provide the
+            // specifics of any sort of movement.
             currentPos.y = y;
             symbol.transform.position = currentPos;
             symbolPosition += symbolEnterPosition;
         }
 
-        topSymbolPosition += 0.01f;
+        // TODO: Deal with the speed of a spin.
+        // TODO: Deal with moving backwards / forwards (possibly based on user interaction).
+        topSymbolPosition += speed;
 
-        if (layoutCurve.Evaluate(symbolPosition - symbolEnterPosition) <= -10.0f)
+        // TODO: Need to implement a pool of objects / symbols.
+        // TODO: Probably want to use a linked list to be able to remove from head and tail efficiently.
+        // TODO: Reduce the curve Evaluate method calls
+        if (layoutCurve.Evaluate(symbolPosition - symbolEnterPosition) <= -5.0f)
         {
             // Add a new symbol to the start of the list.
-            GameObject symbol = Instantiate(symbolPrefab, new Vector3(0, layoutCurve.Evaluate(symbolEnterPosition), -1), Quaternion.identity) as GameObject;
+            topSymbolPosition -= symbolEnterPosition;
+
+            GameObject symbol = Instantiate(symbolPrefab, new Vector3(0, layoutCurve.Evaluate(topSymbolPosition), -1), Quaternion.identity) as GameObject;
             symbol.transform.parent = gameObject.transform;
             symbolObjects.Insert(0, symbol);
 
             // Remove the last symbol.
             Object.DestroyImmediate(symbolObjects[symbolObjects.Count - 1]);
             symbolObjects.RemoveAt(symbolObjects.Count - 1);
-
-            topSymbolPosition = symbolEnterPosition;
         }
     }
 }
